9b70f32ea5b0ca2d3d24e326b8c5ce547fced2f7
Francisco Agustin Munoz <franmunoz2501@gmail.com>
2023-12-13 21:31:39 -0300

    prueba preentrega nueva

--- preentrega2/js/index.js
+++ preentrega2/js/index.js
@@ -1,26 +1,13 @@
 let opcion;
-
-do {
-    alert("Bienvenido al programa, ingrese alguna opcion:")
-    alert("1- Listar productos")
-    opcion = parseInt(prompt("Ingrese un numero: "))
-    switch (opcion) {
-        case 1:
-            alert("Hola")
-            break
-        case 2:
-            alert("Chau")
-            break
-
-
-    }
-} while (opcion != 3)
-
 const PRODUCTOS = [
     { marca: "Nox", precio: 3000 },
     { marca: "Head", precio: 2400 },
     { marca: "Babolat", precio: 2700 }
 ]
+
+function mostrarProductos(PRODUCTOS) {
+    console.log(PRODUCTOS)
+}
 
 let carrito = []
 
@@ -28,8 +15,27 @@ let precioAscendente = PRODUCTOS.sort((a, b) =>
     a.precio - b.precio
 )
 
-console.log(precioAscendente)
+// console.log(precioAscendente)
 
-carrito.push(PRODUCTOS[1])
+// carrito.push(PRODUCTOS[1])
 
-console.log(carrito)
+// console.log(carrito)
+
+alert("Bienvenido al programa, los resultados se mostraran en consola.")
+
+do {
+    alert("Ingrese alguna opcion:")
+    alert("1- Listar productos | 2- Listar productos en forma ascendente | 3- Salir")
+    opcion = parseInt(prompt("Ingrese un numero: "))
+    switch (opcion) {
+        case 1:
+            mostrarProductos(PRODUCTOS);
+            break
+        case 2:
+            console.log(precioAscendente)
+            break
+
+
+    }
+} while (opcion != 3)
+
